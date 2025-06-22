c6d642bc1579ef11a31c41eaaf744c44b1a9454b
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2025-06-22 18:16:28 +0200

    Updates UI and dependencies for improved UX
    Improves the user interface with updated styling, font sizes, and selection colors.
    
    Updates dependencies, including `@radix-ui/react-tooltip` and `lucide-react`, to the latest versions, addressing potential bugs and providing access to the newest features.
    
    Enhances navbar layout, providing a more responsive and accessible design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+
 import Aurora from "../bits/Backgrounds/Aurora/Aurora";
 
 export default function HomePage() {
   return (
     <main className=" min-h-[100vh] w-full">
-      <section className="h-[90vh] w-full relative gap-8 flex justify-center items-center flex-col">
-        <Aurora colorStops={["#a020fa", "#c010f0", "#a020a0"]} amplitude={2} speed={0.4} blend={1}/>
-        <h1 className="text-5xl font-display font-semibold">
-        Effortless SEO Optimization with AI API Integration.
+      <section className="h-[90vh] px-4 w-full relative gap-8 flex justify-center items-center flex-col">
+        <Aurora
+          colorStops={["#a020fa", "#c010f0", "#a020a0"]}
+          amplitude={2}
+          speed={0.4}
+          blend={1}
+        />
+        <h1 className="text-3xl md:text-5xl font-display font-semibold">
+          Effortless <strong className="text-violet-700 font-semibold">SEO Optimization</strong>{" "}
+          with AI API Integration.
         </h1>
-        <p className="w-full max-w-3xl text-xl font-semilight font-body text-center py-2">
-        Instantly optimize your content for search engines using advanced AI. Fast, easy integration for developers and marketers.
+        <p className="w-full max-w-3xl text-base md:text-xl font-semilight font-body md:text-center py-2">
+          Instantly optimize your content for search engines using advanced AI.
+          Fast, easy integration for developers and marketers.
         </p>
       </section>
     </main>
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,38 +1,41 @@
 "use client";
 
 import Link from "next/link";
-import { AnimatePresence, motion } from "motion/react";
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
+import { BookOpenTextIcon, HomeIcon, LucideIcon } from "lucide-react";
 interface NavItem {
   href: string;
   label: string;
+  icon: LucideIcon;
 }
 export default function Navbar() {
   const items: NavItem[] = [
-    { href: "/", label: "Khrone SEO" },
-    { href: "/pricing", label: "Pricing" },
-    { href: "/docs", label: "Documentation" },
+    { href: "/", label: "Home", icon: HomeIcon },
+    { href: "/docs", label: "Documentation", icon: BookOpenTextIcon },
   ];
 
   return (
-    <nav
-      className="w-full flex items-center group justify-center pt-4 h-fit fixed z-10"
-    >
-      <AnimatePresence mode="wait">
-        <motion.div className="glass-container group-hover:max-w-xl max-w-md w-full flex flex-row justify-center items-center gap-6">
+    <nav className="w-full flex items-center rounded-none justify-evenly not-md:glass-container md:justify-between flex-col md:flex-row md:px-12 md:pt-4 h-fit fixed z-10">
+        <motion.div className="glass-container not-md:rounded-none not-md:w-full not-md:py-2  md:px-12 flex flex-row justify-center items-center gap-6">
+          <Link className={cn("nav-link font-semibold text-gray-50")} href={"https://khrone.tech/"}>
+            Khrone
+          </Link>
+        </motion.div>
+        <motion.div className="glass-container group not-md:rounded-none not-md:w-full py-4 md:max-w-md w-full flex flex-row justify-center items-center gap-6">
           {items.map((i: NavItem) => (
-            <motion.p key={i.href} className="relative px-4 py-2 group">
-              <Link
-                key={i.href}
-                className={cn("nav-link")}
-                href={i.href}
-              >
-                {i.label}
-              </Link>
-            </motion.p>
+
+            <Link title={i.label} key={i.href} className={cn("nav-link")} href={i.href}>
+              {i.icon && <i.icon className="" aria-label={i.label} />}
+              <span className="sr-only">{i.label}</span>
+            </Link>
           ))}
         </motion.div>
-      </AnimatePresence>
+        <motion.div className=" pt-2 not-md:border-b-accent not-md:w-full px-12 flex flex-row justify-center items-center gap-6">
+          <Link className={cn("nav-link font-semibold text-gray-50")} href={"/dashboard"}>
+            Dashboard
+          </Link>
+        </motion.div>
     </nav>
   );
 }
